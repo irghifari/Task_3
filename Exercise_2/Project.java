f87d6436c40d240f886da9c19e0e645a17cc0f7f
irghifari <irghifari@gmail.com>
2016-02-03 00:32:08 +0700

    Update Project.java

--- Exercise_2/Project.java
+++ Exercise_2/Project.java
@@ -1,1 +1,45 @@
+//IchsanRachmanGhifari_1301140299_IF3809//
 
+public class Project {
+	private String projectName;
+	private Member[] teamMember;
+	private int nTeam = 0;
+	private boolean releaseStatus;
+	
+	public Project(String projectName) 
+	{
+		this.projectName = projectName;
+		teamMember = new Member[5];
+		releaseStatus = false;
+	}
+	
+	public void addMember(Member m) 
+	{
+		teamMember[nTeam] = m;
+		nTeam++;
+	}
+	
+	public boolean isReleased() 
+	{
+		return releaseStatus;
+	}
+	
+	public void releaseApp() 
+	{
+		releaseStatus = true;
+	}
+	
+	public String toString() 
+	{
+		String condition;
+		if (releaseStatus = true) 
+		{
+			condition = "Released";
+		}
+		else 
+		{
+			condition = "in progress"; 
+		}
+		return ("Projek "+projectName+" status "+condition+" dengan tim "+nTeam);
+	}
+}
