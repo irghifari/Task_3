0a8f82d345068fa7e51cdae2e9840137e3836e9f
irghifari <irghifari@gmail.com>
2016-02-03 00:18:08 +0700

    Update Driver.java

--- Exercise_1/Driver.java
+++ Exercise_1/Driver.java
@@ -1,1 +1,20 @@
+//IchsanRachmanGhifari_1301140299_IF3809//
 
+public class Driver{
+	public static void main(String[] args)
+	{
+		AppStore apps = new AppStore();
+		apps.createApp("aplikasi 1",100);
+		apps.createApp("aplikasi 2",200);
+		apps.createApp("aplikasi 3",300);
+		apps.createApp("aplikasi 4",400);
+		System.out.println(apps.toString());
+		System.out.println(apps.getApp(2).toString());
+		
+		SmartPhone sm = new SmartPhone();
+		sm.setMemory(300);
+		System.out.println(sm.toString());
+		sm.addApplication(apps, 1);
+		System.out.println(sm.toString());
+	}
+}
