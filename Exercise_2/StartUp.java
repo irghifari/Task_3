e6689b89a42bfbcc9253547c2ba2a2118c65cbb5
irghifari <irghifari@gmail.com>
2016-02-03 00:32:52 +0700

    Update StartUp.java

--- Exercise_2/StartUp.java
+++ Exercise_2/StartUp.java
@@ -1,1 +1,59 @@
+//IchsanRachmanGhifari_1301140299_IF3809//
 
+public class StartUp {
+	private Member[] member = new Member[100];
+	private Project[] projectList = new Project[100];
+	private int nMember=0;
+	private int nProject=0;
+	
+	public void addMember(Member m) 
+	{
+		member[nMember] = m; 
+		nMember++;
+	} 
+	
+	public Member getMember(int id) 
+	{
+		return member[id];
+	}
+	
+	public void createProject(String ProjectName) 
+	{
+		Project p = new Project(ProjectName);
+		projectList[nProject] = p;
+		nProject++;
+	}
+	
+	public Project getProject(int id)
+	{
+		return projectList[id];
+	}
+	
+	public void setProjectMember(Project p, Member m) {
+		int pw = m.getProjectWorked();
+		pw++;
+		p.addMember(m);
+		m.setProjectWorked(pw);
+	}
+	
+	public void  releaseProject(Project p) 
+	{
+		p.releaseApp();
+	}
+	
+	public int getNumReleasedProject() {
+		int jumlah;
+		int i;
+		jumlah = 0;
+		i = 0;
+		while (projectList[i] != null) 
+		{
+			if (projectList[i].isReleased()) 
+			{
+				jumlah++;
+			}
+			i++;
+		}
+		return jumlah;
+	}
+}
