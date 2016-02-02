118de8e571d419da8220bdda59d5829b08713c03
irghifari <irghifari@gmail.com>
2016-02-03 00:18:53 +0700

    Update SmartPhone.java

--- Exercise_1/SmartPhone.java
+++ Exercise_1/SmartPhone.java
@@ -1,1 +1,51 @@
+//IF-38-09_Adzan Nugraha_1301140249
 
+public class SmartPhone{
+	private Application[] AppList = new Application[10];
+	private int totalApp;
+	private int memory;
+	
+	public void setMemory(int memory)
+	{
+		this.memory = memory;
+	}
+	
+	public Application getApp(int id)
+	{
+		return AppList[id];
+	}
+	
+	public int getTotalApp()
+	{
+		return totalApp;
+	}
+	
+	public int getRemainingSize()
+	{
+		int mr = memory;
+		int size = 0;
+		for (int i = 0;i<AppList.length;i++)
+		{
+			if(AppList[i] != null)
+			{
+				size = size + AppList[i].getUkuranAplikasi();
+			}
+		}
+		mr = mr-size;
+		return mr;
+	}
+	
+	public void addApplication(AppStore appStore, int apsId)
+	{
+		if(getRemainingSize()>=appStore.getApp(apsId).getUkuranAplikasi())
+		{
+			AppList[totalApp] = appStore.getApp(apsId);
+			totalApp++;
+		}
+	}
+	
+	public String toString()
+	{
+		return ("Ukuran memory"+memory+", memory tersisa : "+getRemainingSize());
+	}
+}
