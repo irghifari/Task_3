c4756dcb0356930bb1c800297829a743e6b1dcc0
irghifari <irghifari@gmail.com>
2016-02-03 00:17:27 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,27 @@
+//IchsanRachmanGhifari_1301140299_IF3809//
 
+public class Application{
+	private String NamaAplikasi;
+	private int UkuranAplikasi;
+	
+	public void Application(String NamaAplikasi, int UkuranAplikasi)
+	{
+		this.NamaAplikasi = NamaAplikasi;
+		this.UkuranAplikasi = UkuranAplikasi;
+	}
+	
+	public String getNamaAplikasi()
+	{
+		return NamaAplikasi;
+	}
+	
+	public int getUkuranAplikasi()
+	{
+		return UkuranAplikasi;
+	}
+	
+	public String toString()
+	{
+		return("Nama Aplikasi : "+NamaAplikasi+", Ukuran Aplikasi : "+UkuranAplikasi);
+	}
+}
